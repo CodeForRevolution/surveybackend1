9069d88e67413a9c6db21a96ad0e7887052c5697
shakirBunna <shakir973019@gmail.com>
2023-12-27 18:35:58 +0530

    change

--- backend/app.js
+++ backend/app.js
@@ -1,11 +1,27 @@
 const express=require("express");
 const app=express();
 const cookieParser =require("cookie-parser");
+
+
+
 const cors=require("cors")
-app.use(cors({ 
-    origin: 'http://localhost:3000', 
-    credentials: true 
+
+
+// app.use(cors({ 
+//     origin: 'http://localhost:3000', 
+//     credentials: true 
+//   }));
+
+
+  const allowedOrigins = ['http://localhost:3000', 'https://survey-xi-liart.vercel.app', ];
+
+  app.use(cors({
+    origin: allowedOrigins,
+    credentials: true,
   }));
+
+
+
 app.use(express.json());
 app.use(cookieParser());
 
