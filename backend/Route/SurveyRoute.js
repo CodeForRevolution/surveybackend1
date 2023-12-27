ce046d467d4d1925f9bc46e89e90654a4d4e7718
shakirBunna <shakir973019@gmail.com>
2023-12-27 18:54:23 +0530

    improve

--- backend/Route/SurveyRoute.js
+++ backend/Route/SurveyRoute.js
@@ -4,7 +4,7 @@ const express=require('express');
 const { createSurvey, getSurvey } = require('../Controllers/SurveyController');
 const { isAuthenticatedUser } = require('../utils/auth');
 const router=express.Router();
-router.route("/survey").get( isAuthenticatedUser, getSurvey)
+router.route("/survey").get(getSurvey)
 router.route("/survey/new").post(createSurvey);
 
 
